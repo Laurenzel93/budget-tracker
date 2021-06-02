f77246d65710045c1c9ee3d2dea5b25cbe51cc93
Lauren Wenzel <Laurenzel93@aol.com>
2021-06-02 18:59:54 -0400

    updating service worker file

--- public/service-worker.js
+++ public/service-worker.js
@@ -21,7 +21,6 @@ self.addEventListener('install', (event) => {
     );
 });
 
-// The activate handler takes care of cleaning up old caches.
 self.addEventListener('activate', (event) => {
     const currentCaches = [PRECACHE, RUNTIME];
     event.waitUntil(
